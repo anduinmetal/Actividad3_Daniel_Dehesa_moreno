6bf803879db273ee0011741c1874ea1b9e34dfb7
Dani <Dani@DANI-CASA>
2017-12-14 10:24:12 +0100

    Proyecto nuevo

--- src/Actividad3_Daniel_Dehesa.java
+++ src/Actividad3_Daniel_Dehesa.java
@@ -3,7 +3,7 @@ public class Actividad3_Daniel_Dehesa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String frase="Mi primer proyecto en Eclipse.";
+		String frase="Mi primer proyecto en Eclipse";
 	}
 
 }
